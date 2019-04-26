da32deff6ca88ca1f57f131a95535853ee10d487
TeamPlayer21 <teamplayer21@tutanota.com>
2019-04-26 00:10:36 -0400

    added .gitignore and removed virtual from signal declarations

--- tor_config_options_interface.h
+++ tor_config_options_interface.h
@@ -20,14 +20,14 @@ public:
     virtual ~tor_config_options_interface() { }
 
 signals:
-    virtual void send_completed_save_to_configfile(bool);
-    virtual void send_config_settings(QString config_option, QByteArray);
-    virtual void send_countries_map(QMap<QString, QString>);
-    virtual void send_populated_country_list(QString, QStringList, QStringList);
-    virtual void send_synced_enforce_distinct_subnets_with_torrc(bool);
-    virtual void send_synced_num_gaurds_with_torrc(int);
-    virtual void send_synced_strictnodes_with_torrc(bool);
-    virtual void send_syned_use_gaurds_with_torrc(bool);
+    void send_completed_save_to_configfile(bool);
+    void send_config_settings(QString config_option, QByteArray);
+    void send_countries_map(QMap<QString, QString>);
+    void send_populated_country_list(QString, QStringList, QStringList);
+    void send_synced_enforce_distinct_subnets_with_torrc(bool);
+    void send_synced_num_gaurds_with_torrc(int);
+    void send_synced_strictnodes_with_torrc(bool);
+    void send_syned_use_gaurds_with_torrc(bool);
 
 public slots:
     virtual void save_to_configfile() = 0;
